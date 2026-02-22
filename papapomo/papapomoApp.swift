fa948ba76e3665ff2e6d81a1c05400eb1650454d
Jatin Singh <singhjatin1812@gmail.com>
2026-02-22 20:01:04 +0530

    Initial Commit

---
+++ papapomo/papapomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  papapomoApp.swift
+//  papapomo
+//
+//  Created by Jatin Singh on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct papapomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ papapomoTests/papapomoTests.swift
@@ -0,0 +1,17 @@
+//
+//  papapomoTests.swift
+//  papapomoTests
+//
+//  Created by Jatin Singh on 22/02/26.
+//
+
+import Testing
+@testable import papapomo
+
+struct papapomoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
